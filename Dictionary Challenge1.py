d907428e68f4c4f12d825c552a0656169bb62f5b
Ravi Dubey <38419795+ravi0dubey@users.noreply.github.com>
2022-04-22 21:06:50 +0000

    Dictionary Challenge

---
+++ Dictionary Challenge1.py
@@ -0,0 +1,14 @@
+
+Capital = {"France":"Paris", "Germany":"Berlin","Italy":"Rome"}
+
+Vlog = {
+  "France":["Paris", "Nice","Deauville"],
+  "Germany":["Frankfurt","Berlin","Munich"],
+  "Italy":["Rome","Venice","Milan"]
+       }
+
+Vlog = {
+  "France":{"Cities_Visited":["Paris","Nice","Deauville"],"No_Of Visit" : [4,2,6]}
+
+}
+print(Vlog)
--- main.py
+++ main.py
@@ -1,41 +1,28 @@
-Error_dictionary = {
-  "Bug1":"Userid does not exist",
-  "Bug2": "Userid and Password does not exist",
+
+Capital = {
+  "France":"Paris"
+ # "Germany":"Berlin",
+ # "Italy":"Rome"
 }
 
-#print(Error_dictionary) # Print all contents of Dictionary
-#print(Error_dictionary["Bug2"]) #Print value from Dictionary for Id = Bug2
+Vlog1 = {
+  "France2":["Paris", "Nice","Deauville"]
+  #"Germany":["Frankfurt","Berlin","Munich"],
+  #"Italy":["Rome","Venice","Milan"]
+}      
 
-#Error_dictionary["Bug1"] ="Userid not found" #Error Description of Bug1 gets updated 
+Vlog2 = {
+  "France":{"Cities_Visited":["Paris","Nice","Deauville"],"No_of Visit" : [4,2,6]},
+  "Germany":{"Cities_Visited":["Frankfurt","Berlin","Munich"],"No_of Visit" : [2,0,0]},
+  "Italy":{"Cities_Visited":["Rome","Venice","Milan"],"No_of Visit" : [1,1,2]},
+      }
+#print(Vlog1)
+#print(Vlog2)
 
-#print(Error_dictionary) # Print all contents of Dictionary including updated content of Bug1
-#print(Error_dictionary["Bug2"]) #Print value from Dictionary for Id = Bug2
+#Vlog4 = {
+# "Radha":{Vlog1}
+#}
+Vlog3 = ["France":"Paris", "France2":{"Cities_Visited":["Paris","Nice","Deauville"],"No_of Visit" : [4,2,6]},
+]
 
-#Error_dictionary["Bug3"] ="Password is not strong" #Bug3 gets added to the list. 
-
-#for Error_key in Error_dictionary:
-  #print(Error_key) #It prints Bug1 and Bug2 which are the key of the Dictionary
-  #print(Error_dictionary[Error_key]) #Error_Key act as subscript to print the content of Error_dictionary 
-  
-Capital = {"France":"Paris", "Germany":"Berlin","Italy":"Rome"}
-Vlog = {
-  "France":["Paris", "Nice","Deauville"],
-  "Germany":["Frankfurt","Berlin","Munich"],
-  "Italy":["Rome","Venice","Milan"]
-       }
-#for country in Vlog:
-#  print(Vlog[country])
-
-Vlog = {"France" : {"Cities_visited":["Paris","Nice","Deauville"], "total_visits": 12},
-        "Germany": {"Cities_visited":["Frankfurt","Berlin","Munich"], "total_visits": 2},
-        "Italy":{"Cities_visited":["Rome","Venice","Milan"], "total_visits": 5}}
-
-#for country in Vlog:
-#  print(Vlog[country])
-
-country = "India"
-Vlog[country]= {"Cities_visited":["Delhi","Mumbai","Chennai"], "total_visits": 15}
-
-country = "Russia"
-Vlog[country] = {"visits": 12,"cities": ["Paris", "Lille", "Dijon"], "aa": 16}
-print(Vlog)
+print(Vlog3)
