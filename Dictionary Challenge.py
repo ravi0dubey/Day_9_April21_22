929bb401ae190753c5a1670467fc9977b077709a
Ravi Dubey <38419795+ravi0dubey@users.noreply.github.com>
2022-04-22 02:55:53 +0000

    Dictionary

---
+++ Dictionary Challenge.py
@@ -0,0 +1,30 @@
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 
+
+def add_new_country(country_name, no_of_visit,city_visited):
+    new_country ={}
+    new_country["country"] = country_name
+    new_country["visits"]= no_of_visit
+    new_country["cities"]= city_visited
+    travel_log.append(new_country)
+
+   
+
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+
+print(travel_log)
--- main.py
+++ main.py
@@ -3,17 +3,39 @@ Error_dictionary = {
   "Bug2": "Userid and Password does not exist",
 }
 
-print(Error_dictionary) # Print all contents of Dictionary
-print(Error_dictionary["Bug2"]) #Print value from Dictionary for Id = Bug2
+#print(Error_dictionary) # Print all contents of Dictionary
+#print(Error_dictionary["Bug2"]) #Print value from Dictionary for Id = Bug2
 
-Error_dictionary["Bug1"] ="Userid not found" #Error Description of Bug1 gets updated 
+#Error_dictionary["Bug1"] ="Userid not found" #Error Description of Bug1 gets updated 
 
-print(Error_dictionary) # Print all contents of Dictionary including updated content of Bug1
-print(Error_dictionary["Bug2"]) #Print value from Dictionary for Id = Bug2
+#print(Error_dictionary) # Print all contents of Dictionary including updated content of Bug1
+#print(Error_dictionary["Bug2"]) #Print value from Dictionary for Id = Bug2
 
-Error_dictionary["Bug3"] ="Password is not strong" #Bug3 gets added to the list. 
+#Error_dictionary["Bug3"] ="Password is not strong" #Bug3 gets added to the list. 
 
-for Error_key in Error_dictionary:
-  print(Error_key) #It prints Bug1 and Bug2 which are the key of the Dictionary
-  print(Error_dictionary[Error_key]) #Error_Key act as subscript to print the content of Error_dictionary 
+#for Error_key in Error_dictionary:
+  #print(Error_key) #It prints Bug1 and Bug2 which are the key of the Dictionary
+  #print(Error_dictionary[Error_key]) #Error_Key act as subscript to print the content of Error_dictionary 
   
+Capital = {"France":"Paris", "Germany":"Berlin","Italy":"Rome"}
+Vlog = {
+  "France":["Paris", "Nice","Deauville"],
+  "Germany":["Frankfurt","Berlin","Munich"],
+  "Italy":["Rome","Venice","Milan"]
+       }
+#for country in Vlog:
+#  print(Vlog[country])
+
+Vlog = {"France" : {"Cities_visited":["Paris","Nice","Deauville"], "total_visits": 12},
+        "Germany": {"Cities_visited":["Frankfurt","Berlin","Munich"], "total_visits": 2},
+        "Italy":{"Cities_visited":["Rome","Venice","Milan"], "total_visits": 5}}
+
+#for country in Vlog:
+#  print(Vlog[country])
+
+country = "India"
+Vlog[country]= {"Cities_visited":["Delhi","Mumbai","Chennai"], "total_visits": 15}
+
+country = "Russia"
+Vlog[country] = {"visits": 12,"cities": ["Paris", "Lille", "Dijon"], "aa": 16}
+print(Vlog)
