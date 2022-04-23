e22d66773b303fc045ea6b243d15bf983a176a31
Ravi Dubey <38419795+ravi0dubey@users.noreply.github.com>
2022-04-23 02:13:44 +0000

    Test

--- Bidding_Challenge.py
+++ Bidding_Challenge.py
@@ -1,29 +1,28 @@
+from replit import clear
 print("Welcome to the secret auction program.\n")
 to_continue = "Yes"
-bidding_dict = {
-  
-#  "Bidders":[ "Farzi",],
-#  "Bid_Amount": [0,]
-  "Bidders":[ ],
-  "Bid_Amount": []
-  
-}
 
-name_list = []
-amount_list= []
+bidding_dict = {}
+
+def check_bid(bidding_record):
+  highest_bidder = ' '
+  highest_bid = 0
+  
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid:
+      highest_bid = bid_amount
+      highest_bidder = bidder
+  print(f"The winner is {highest_bidder} with a bid of ${highest_bid}")
+
 while (to_continue != 'no'):
   name = input("What is your name?: ")
-  name_list.append(name)
-  bid_amount = int(input("What's your Bid?: $"))
-  amount_list.append(bid_amount)
+  amount = int(input("What's your Bid?: $"))
+  bidding_dict[name] = amount
   to_continue = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
+  clear()
 
-  
-bidding_dict["Bidders"]= name_list
-bidding_dict["Bid_Amount"]= amount_list
-
-print(bidding_dict)
-#print(bidding_dict["Bid_amount"].sort())
+check_bid(bidding_dict)
 
 
-#print(f"The winner is {name} with a bid of ${bid_amount}")
+
--- main.py
+++ main.py
@@ -1,32 +1,51 @@
 from replit import clear
 print("Welcome to the secret auction program.\n")
 to_continue = "Yes"
-bidding_dict = {
-  
-#  "Bidders":[ "Farzi",],
-#  "Bid_Amount": [0,]
-  "Bidders":[ ],
-  "Bid_Amount": []
-  
-}
 
-name_list = []
-amount_list= []
+#bidding_dict = {
+#  "Bidders":[ ],
+#  "Bid_Amount": []
+#}
+
+#name_list = []
+#amount_list= []
+#while (to_continue != 'no'):
+#  name = input("What is your name?: ")
+#  name_list.append(name)
+#  amount = int(input("What's your Bid?: $"))
+#  amount_list.append(amount)
+#  to_continue = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
+#  clear()
+  
+#bidding_dict["Bidders"]= name_list
+#bidding_dict["Bid_Amount"]= amount_list
+#print(bidding_dict)
+#bidding_dict["Bid_Amount"].sort()
+#print(bidding_dict)
+
+bidding_dict = {}
+
+
+
+def check_bid(bidding_record):
+  highest_bidder = ' '
+  highest_bid = 0
+  
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid:
+      highest_bid = bid_amount
+      highest_bidder = bidder
+  print(f"The winner is {highest_bidder} with a bid of ${highest_bid}")
+
 while (to_continue != 'no'):
   name = input("What is your name?: ")
-  name_list.append(name)
   amount = int(input("What's your Bid?: $"))
-  amount_list.append(amount)
+  bidding_dict[name] = amount
   to_continue = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
   clear()
-  
-bidding_dict["Bidders"]= name_list
-bidding_dict["Bid_Amount"]= amount_list
 
-print(bidding_dict)
-bidding_dict["Bid_Amount"].sort()
-print(bidding_dict)
-len1 =len(bidding_dict["Bid_Amount"])
-high_bid_amount = bidding_dict["Bid_Amount"][len1-1]
+check_bid(bidding_dict)
 
-print(f"The winner is {name} with a bid of ${high_bid_amount}")
+
+
