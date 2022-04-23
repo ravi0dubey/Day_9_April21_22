94d160f71c6a160bb7fd22242cfeff7079bbcfa7
Ravi Dubey <38419795+ravi0dubey@users.noreply.github.com>
2022-04-23 01:43:01 +0000

    Challenge

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from replit import clear
 print("Welcome to the secret auction program.\n")
 to_continue = "Yes"
 bidding_dict = {
@@ -14,16 +15,18 @@ amount_list= []
 while (to_continue != 'no'):
   name = input("What is your name?: ")
   name_list.append(name)
-  bid_amount = int(input("What's your Bid?: $"))
-  amount_list.append(bid_amount)
+  amount = int(input("What's your Bid?: $"))
+  amount_list.append(amount)
   to_continue = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
-
+  clear()
   
 bidding_dict["Bidders"]= name_list
 bidding_dict["Bid_Amount"]= amount_list
 
 print(bidding_dict)
-#print(bidding_dict["Bid_amount"].sort())
+bidding_dict["Bid_Amount"].sort()
+print(bidding_dict)
+len1 =len(bidding_dict["Bid_Amount"])
+high_bid_amount = bidding_dict["Bid_Amount"][len1-1]
 
-
-#print(f"The winner is {name} with a bid of ${bid_amount}")
+print(f"The winner is {name} with a bid of ${high_bid_amount}")
